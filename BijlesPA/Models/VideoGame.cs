8f7de51c955ef190ef0e4ef5c987c6f593a1841d
tubegamerxljens <r1057412@student.thomasmore.be>
2025-03-24 14:45:42 +0100

    Modellen en repository toegevoegd

---
+++ BijlesPA/Models/VideoGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BijlesPA.Models
+{
+    public class VideoGame
+    {
+        public int Id { get; set; }
+        public string Genre { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Discount { get; set; }
+
+        public Developer CurrentDeveloper { get; set; }
+
+    }
+}
---
+++ BijlesPA/Repos/VideoGameRepo.cs
@@ -0,0 +1,43 @@
+﻿using BijlesPA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BijlesPA.Repos
+{
+    public class VideoGameRepo
+    {
+        private List<VideoGame> Mygames = new List<VideoGame>();
+
+        VideoGame Eldenring = new VideoGame()
+        {
+            Id = 1,
+            Name = "Eldenring",
+            Genre = "Actie",
+            Price = 30,
+            Discount = 15
+
+        };
+        VideoGame Ark = new VideoGame()
+        {
+            Id = 2,
+            Name = "Ark",
+            Genre = "Survival",
+            Price = 35,
+            Discount = 10
+
+        };
+
+        public VideoGameRepo()
+        {
+            Mygames.Add(Eldenring);
+            Mygames.Add(Ark);
+        }
+
+
+    }
+
+    //repository: klasse die communiceert met een datasource.
+}
